a938813d3915c1110755791c9ac8ed579d97d248
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2024-06-26 15:24:21 +0200

    chore: update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,7 @@ export default function Header(): React.JSX.Element {
         <div className="fixed top-10 left-1/2 transform -translate-x-1/2 text-center w-full md:w-2/3 lg:w-1/2 xl:w-1/2">
             <h1 className="font-bold text-3xl">Local Chat</h1>
             <p className="mt-3 text-lg">
-                A demo using <code>window.ai</code> to chat with Gemini without
-                the internet
+                A demo using <code>window.ai</code> to chat with Gemini locally
             </p>
         </div>
     );
