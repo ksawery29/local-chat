4deba4d34dea02a33707cdedfb1caff4e27e4d6e
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2024-06-26 11:30:25 +0200

    feat: check if browser supports window.ai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 import NotSupported from "@/components/not-supported";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main>
-      <p>hello world</p>
-      <NotSupported />
-    </main>
-  );
+    // check if browser supports window.ai
+    if (typeof window.ai === "undefined") {
+        return <NotSupported />;
+    }
+
+    const [supports, setSupports] = useState<boolean>(true);
+    useEffect(() => {
+        (async () => {
+            if ((await window.ai.canCreateTextSession()) !== "readily") {
+                setSupports(false);
+            }
+        })();
+    }, []);
+
+    if (!supports) {
+        return <NotSupported />;
+    }
+
+    return (
+        <main>
+            <p>hello world</p>
+        </main>
+    );
 }
